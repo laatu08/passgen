421e6af2efbe5ab47c387976c53262f48bdb78d9
laatu08 <partha2002borah@gmail.com>
2025-12-28 23:27:16 +0530

    boolean flag added

--- cmd/main.go
+++ cmd/main.go
@@ -10,9 +10,13 @@ import (
 
 func main() {
 	length := flag.Int("length", 12, "Password length")
+	upper := flag.Bool("upper", false, "Include uppercase letters")
+	lower := flag.Bool("lower", false, "Include lowercase letters")
+	digits := flag.Bool("digits", false, "Include digits")
+	symbols := flag.Bool("symbols", false, "Include symbols")
 	flag.Parse()
 
-	password, err := generator.Generate(*length)
+	password, err := generator.Generate(*length, *upper, *lower, *digits, *symbols)
 	if err != nil {
 		fmt.Println("Error:", err)
 		os.Exit(1)
--- internal/generator/password.go
+++ internal/generator/password.go
@@ -3,11 +3,43 @@ package generator
 import (
 	"crypto/rand"
 	"math/big"
+	"errors"
 )
 
-const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()"
+// const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()"
 
-func Generate(length int)(string, error){
+const (
+	uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	lowercase = "abcdefghijklmnopqrstuvwxyz"
+	digitsSet = "0123456789"
+	symbolsSet = "!@#$%^&*()-_=+[]{}<>?/|"
+)
+
+func Generate(length int, upper, lower, digits, symbols bool)(string, error){
+	if length <= 0 {
+		return "", errors.New("length must be greater than 0")
+	}
+	
+	charset := ""
+
+	if upper {
+		charset += uppercase
+	}
+	if lower {
+		charset += lowercase
+	}
+	if digits {
+		charset += digitsSet
+	}
+	if symbols {
+		charset += symbolsSet
+	}
+
+	if charset == "" {
+		charset="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()"
+		// return "", errors.New("select at least one character type")
+	}
+
 	password:=make([]byte, length)
 
 	for i := 0; i < length; i++ {
